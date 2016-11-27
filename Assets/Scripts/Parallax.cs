923fbef84e996c718b58d4b9e4bf4110a595f17a
Rugile Bendinskaite <rugile.bendinskaite@gmail.com>
2016-11-27 15:23:43 +0200

    Pakeistas scroll'inimas

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallax : MonoBehaviour
+{
+    public float speed;
+    public float start = 0;
+
+    Vector2 offset = new Vector2(0, 0);
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    void Update()
+    {
+        offset.y = start + Time.time * speed;
+        rend.material.mainTextureOffset = offset;
+    }
+}
